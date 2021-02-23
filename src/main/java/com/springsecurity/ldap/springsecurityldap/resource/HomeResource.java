70899df7d177876db8d8ba4eeeceafed8e500a84
Amrita25 <amrita.majumdar25@gmail.com>
2021-02-24 02:10:01 +0530

    LDAP configuration code changes

---
+++ src/main/java/com/springsecurity/ldap/springsecurityldap/resource/HomeResource.java
@@ -0,0 +1,14 @@
+package com.springsecurity.ldap.springsecurityldap.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+	
+	@GetMapping("/")
+	public String index(){
+		return "Home Page";
+	}
+
+}
